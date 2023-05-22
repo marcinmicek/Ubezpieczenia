bcdab910ca5d5114aa66e862f329e64ca1439c0b
Marcin Micek <marcin.micek@gmail.com>
2023-05-22 12:29:13 +0200

    Minor refactoring

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -32,7 +32,7 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
 	}
 }
 
